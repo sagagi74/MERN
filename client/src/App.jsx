c1eedaa7bd32f58bd198b6caaba680bffc12f511
yong woo Yun <yongwooyun@hotmail.com>
2024-07-24 12:08:36 -0700

    verify app and main again before testng

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css';
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { Outlet } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
+
   const token = localStorage.getItem('id_token');
+
   return {
     headers: {
       ...headers,
@@ -22,6 +30,7 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
 
 function App() {
   return (
