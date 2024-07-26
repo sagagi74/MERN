bbecb43e1e5bc81623168eef0503775045131e82
yong woo Yun <yongwooyun@hotmail.com>
2024-07-26 12:56:21 -0700

    adding comment line on mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,5 @@
 import { gql } from '@apollo/client';
-
+// Mutation to log in a user
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -20,7 +20,7 @@ export const LOGIN_USER = gql`
     }
   }
 `;
-
+// Mutation to add a new user
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -41,7 +41,7 @@ export const ADD_USER = gql`
     }
   }
 `;
-
+// Mutation to save a book to a user's saved books
 export const SAVE_BOOK = gql`
   mutation saveBook($input: SaveBookInput!) {
     saveBook(input: $input) {
@@ -59,7 +59,7 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
-
+// Mutation to remove a book from a user's saved books
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
